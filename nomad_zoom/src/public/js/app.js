4cf0caf495b348b8a396ab19c1732b866427f24d
Martina Lee <martinalee0528@gmail.com>
2021-11-01 19:26:28 +0900

    offer, answer, icecandidate

--- nomad_zoom/src/public/js/app.js
+++ nomad_zoom/src/public/js/app.js
@@ -3,10 +3,14 @@ const myFace = document.querySelector('#myFace');
 const muteBtn = document.querySelector('#mute');
 const cameraBtn = document.querySelector('#camera');
 const camerasSelect = document.querySelector('#cameras');
+const call = document.querySelector("#call")
 
+call.hidden = true;
 let myStream;
 let muted = false;  //mute와 카메라 상태를 체크하는 변수
 let cameraOff = false;
+let roomName;
+let myPeerConnection;
 
 async function getCameras(){
     try{
@@ -47,7 +51,7 @@ async function getMedia(deviceId){
         console.log(e);
     }
 }
-getMedia();
+
 
 function handleMuteClick(){
     myStream.getAudioTracks().forEach((track)=>{
@@ -81,3 +85,66 @@ async function handleCameraChange(){
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+
+
+//welcome form (choose a room)
+const welcome = document.querySelector("#welcome")
+welcomeForm = welcome.querySelector('form');
+async function initCall(){
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+async function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector('input');
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value ="";
+}
+welcomeForm.addEventListener('submit',handleWelcomeSubmit);
+
+socket.on("welcome", async ()=>{ //원래 방ㅇ ㅔ참가해 있던 사람
+    console.log('somebody joined');
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    socket.emit("offer", offer, roomName);
+    console.log('sent the offer');
+})
+
+socket.on("offer", async (offer)=>{ //새로 방에 입장한 사람
+    console.log("received the offer")
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer");
+})
+
+socket.on("answer", answer =>{
+    console.log("recived the answer");
+    myPeerConnection.setRemoteDescription(answer);
+})
+socket.on('ice', ice =>{
+    myPeerConnection.addIceCandidate(ice);
+})
+//rtc code
+function makeConnection(){
+    myPeerConnection = new RTCPeerConnection(); //ptp 연결만듦
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream.getTracks().forEach(track=>myPeerConnection.addTrack(track, myStream)); //양쪽 비디오 오디오 정보를 받아왔음
+}
+
+function handleIce(data){
+    console.log('sent candidate');
+    socket.emit('ice', data.candidate, roomName);
+}
+function handleAddStream(data){
+    const peersFace = document.querySelector("#peersFace");
+    console.log("got an event from my peer");
+    peersFace.srcObject = data.stream;
+}
--- nomad_zoom/src/server.js
+++ nomad_zoom/src/server.js
@@ -20,3 +20,19 @@ const handleListen = () => {
 const httpserver = http.createServer(app);
 const wsServer = new Server(httpserver);
 httpserver.listen(3000, handleListen);
+
+wsServer.on("connection", socket=>{
+    socket.on("join_room",(roomName) =>{
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    })
+    socket.on("offer", (offer, roomName)=>{
+        socket.to(roomName).emit('offer',offer);
+    });
+    socket.on("answer", (answer, roomName)=>{
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName)=>{
+        socket.to(roomName).emit("ice", ice);
+    });
+})
