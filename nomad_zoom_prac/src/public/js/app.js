71d5a2cf4f3a4eb5f58e9a1793678f528b0d670b
Martina Lee <martinalee0528@gmail.com>
2021-11-04 21:50:52 +0900

    3.1 mute/cameraoff, button image, select camera

--- nomad_zoom_prac/src/public/js/app.js
+++ nomad_zoom_prac/src/public/js/app.js
@@ -9,38 +9,62 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 
+
 async function getMedia(){
     try{
         myStream = await navigator.mediaDevices.getUserMedia({
             audio: true,
             video: true,
         });
-        console.log(myStream);
+        //console.log(myStream);
         myFace.srcObject = myStream;
+        await getCameras();
     } catch(e){
         console.log(e)
     }
 }
 
 getMedia();
+
+async function getCameras(){
+    try{
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const cameras = devices.filter(device => device.kind === 'videoinput')
+        cameras.forEach((camera)=>{
+            const option = document.createElement('option');
+            option.value = camera.deviceId;
+            option.innerText = camera.label;
+            camerasSelect.appendChild(option);
+        })
+        console.log("devices", devices)
+    } catch(e){
+        console.log(e)
+    }
+}
+
 function handleMuteStatus(event){
     event.preventDefault();
-    if(muted){
-        muteBtn.innerText = "마이크 끄기";
+    myStream.getAudioTracks().forEach(track => track.enabled = !track.enabled );
+    const micImg = muteBtn.querySelector('img');
+    if(muted){ 
+        micImg.setAttribute("src", "/public/assets/unmute.png");
     }
-    else{
-        muteBtn.innerText = "마이크 켜기";
+    else{ 
+        micImg.setAttribute("src", "/public/assets/mute.png");
     }
-    muted = !muted;
+    muted = !muted;    
 }
 
 function handleCamStatus(event){
     event.preventDefault();
-    if(cameraOff){
-        cameraBtn.innerText = "카메라 끄기";
+    console.log(myStream.getVideoTracks())
+    myStream.getVideoTracks().forEach(track => track.enabled = !track.enabled )
+    const camImg = cameraBtn.querySelector('img');
+    if(cameraOff){ 
+        camImg.setAttribute("src", "/public/assets/camon.png");
     }
-    else{
-        cameraBtn.innerText = '카메라 켜기';
+    else{ 
+        camImg.setAttribute("src", "/public/assets/camoff.png");
     }
     cameraOff = !cameraOff;
 }
