5f5fad8b98d1b098d31524d2375eb87fc803e38a
Martina Lee <martinalee0528@gmail.com>
2021-12-02 22:07:25 +0900

    models Post created

--- django/codeit/costory/costory/posts/models.py
+++ django/codeit/costory/costory/posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 50)
+    content = models.TextField()
+    dt_created = models.DateTimeField(verbose_name = 'Date created', auto_now_add=True) #별명붙이기!, first created time
+    dt_modified = models.DateTimeField(verbose_name='Date modified', auto_now=True) #last modified moment
+
+    def __str__(self):
+        return self.title
