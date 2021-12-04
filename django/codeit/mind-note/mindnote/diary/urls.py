8026ac436dacdf2e4690dcf7805ae87e4232f1b5
Martina Lee <martinalee0528@gmail.com>
2021-12-04 15:26:22 +0900

    mind note info page

--- django/codeit/mind-note/mindnote/diary/urls.py
+++ django/codeit/mind-note/mindnote/diary/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('diary/', views.page_list, name='page-list'),
-    # path('diary/info/', views.info, name='info'),
+    path('diary/info/', views.info, name='info'),
     # path('diary/writes/', views.page_create, name='page-create'),
-    # path('diary/page/<int:page_id>/', views.page_detail, name='page-detail'),
+    path('diary/page/<int:page_id>/', views.page_detail, name='page-detail'),
     # path('diary/page/<int:page_id>/edit/', views.page_update, name='page-edit'),
     # path('diary/page/<int:page_id>/delete/', views.page_delete, name='page-delete'),
 ]
--- django/codeit/mind-note/mindnote/diary/views.py
+++ django/codeit/mind-note/mindnote/diary/views.py
@@ -6,3 +6,11 @@ def page_list(request):
     pages = Page.objects.all()
     context = {'pages':pages}
     return render(request, 'diary/page_list.html', context)
+
+def page_detail(request, page_id):
+    page = Page.objects.get(id=page_id)
+    context = {'page':page}
+    return render(request, 'diary/page_detail.html', context)
+
+def info(request):
+    return render(request, 'diary/info.html')
