2017bab47e8dd10de8be846fe16bcead8b908e85
Martina Lee <martinalee0528@gmail.com>
2021-12-04 19:10:19 +0900

    django validators

--- django/codeit/costory/costory/posts/models.py
+++ django/codeit/costory/costory/posts/models.py
@@ -1,9 +1,12 @@
+from django.core import validators
 from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length = 50, unique=True, error_messages={'unique':'이미있는제목이네요!'})
-    content = models.TextField()
+    content = models.TextField(validators=[MinLengthValidator(10, '10자 이상 적어주세요.'),validate_symbols])
     dt_created = models.DateTimeField(verbose_name = 'Date created', auto_now_add=True) #별명붙이기!, first created time
     dt_modified = models.DateTimeField(verbose_name='Date modified', auto_now=True) #last modified moment
 
