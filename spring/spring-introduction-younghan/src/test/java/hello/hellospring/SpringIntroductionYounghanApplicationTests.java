2143018456b4f35243ae02920c84d244ed465179
Martina Lee <martinalee0528@gmail.com>
2022-01-23 21:33:00 +0900

    feat: Member 테스트 코드 추가
    Member 클래스 테스트 추가

---
+++ spring/spring-introduction-younghan/src/test/java/hello/hellospring/SpringIntroductionYounghanApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroductionYounghanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
