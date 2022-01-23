456d0a5e5c405e5d4472d30743b4440ac319aaaf
Martina Lee <martinalee0528@gmail.com>
2022-01-23 20:08:14 +0900

    Setting: 초기 환경 셋팅

---
+++ spring/spring-introduction-younghan/src/main/java/hello/hellospring/SpringIntroductionYounghanApplication.java
@@ -0,0 +1,13 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntroductionYounghanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntroductionYounghanApplication.class, args);
+    }
+
+}
